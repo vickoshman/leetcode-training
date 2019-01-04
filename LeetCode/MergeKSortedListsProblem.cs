8ab470d0b42b5bf0e215ab23187654f6bf6a2614
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-04 20:00:19 +0200

    https://leetcode.com/problems/merge-k-sorted-lists

---
+++ LeetCode/MergeKSortedListsProblem.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+  public class MergeKSortedListsProblem
+  {
+    public ListNode MergeKLists(ListNode[] lists)
+    {
+      if (lists == null || lists.Length == 0)
+        return null;
+
+      var completed = new HashSet<int>();
+      ListNode result = null;
+      ListNode currentResultNode = null;
+
+      while (completed.Count < lists.Length)
+      {
+        int selectedList = 0;
+        ListNode minNode = null;
+
+        for (int i = 0; i < lists.Length; i++)
+        {
+          if (completed.Contains(i))
+            continue;
+
+          if (lists[i] == null)
+          {
+            completed.Add(i);
+            continue;
+          }
+
+          if (currentResultNode != null && currentResultNode.val == lists[i].val)
+          {
+            minNode = lists[i];
+            selectedList = i;
+            break;
+          }
+
+          if (minNode == null || lists[i].val < minNode.val)
+          {
+            minNode = lists[i];
+            selectedList = i;
+          }
+        }
+
+        if (lists[selectedList] == null)
+        {
+          completed.Add(selectedList);
+          continue;
+        }
+
+        lists[selectedList] = lists[selectedList].next;
+        if (lists[selectedList] == null)
+          completed.Add(selectedList);
+
+        if (result == null)
+        {
+          result = minNode;
+          result.next = null;
+          currentResultNode = result;
+        }
+        else
+        {
+          currentResultNode.next = minNode;
+          currentResultNode = currentResultNode.next;
+        }
+      }
+
+      return result;
+    }
+  }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,14 +7,12 @@ namespace LeetCode
   {
     static void Main(string[] args)
     {
-      var head = new ListNode(1);
-      head.next = new ListNode(2);
-      head.next.next = new ListNode(3);
-      head.next.next.next = new ListNode(4);
-      head.next.next.next.next = new ListNode(5);
-      head.next.next.next.next.next = new ListNode(6);
       
-      var result = new SwapNodesInPairsProblem().SwapPairs(head);
+      var lists = new ListNode[]
+      {
+        null, null
+      };
+      var result = new MergeKSortedListsProblem().MergeKLists(lists);
 
       Console.WriteLine();
       Console.ReadKey();
