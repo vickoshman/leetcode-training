7e055e900b0684b88ca934c47ae4c62385859e89
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-05 19:51:09 +0200

    https://leetcode.com/problems/maximum-subarray

---
+++ LeetCode/MaximumSubarrayProblem.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode
+{
+  public class MaximumSubarrayProblem
+  {
+    public int MaxSubArray(int[] nums)
+    {
+      if (nums == null || nums.Length == 0)
+        return 0;
+
+      int maxSum = int.MinValue;
+      for (int i = 0; i < nums.Length; i++)
+      {
+        int currentSum = nums[i];
+        if (currentSum > maxSum)
+          maxSum = currentSum;
+
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+          currentSum += nums[j];
+
+          if (currentSum > maxSum)
+            maxSum = currentSum;
+        }
+      }
+
+      return maxSum;
+    }
+  }
+}
