ab984f9d48a1b1bab98916e10d90a2cd5deddf8e
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-05 13:16:43 +0200

    https://leetcode.com/problems/generate-parentheses

--- LeetCode/GenerateParenthesesProblem.cs
+++ LeetCode/GenerateParenthesesProblem.cs
@@ -1,12 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
 
 namespace LeetCode
 {
   public class GenerateParenthesesProblem
   {
+    public void Backtrack(string current, int n, int open, int close, HashSet<string> result)
+    {
+      if (current.Length == n * 2)
+      {
+        result.Add(current);
+        return;
+      }
+
+      if (open < n)
+        Backtrack(current + "(", n, open + 1, close, result);
+
+      if (close < open)
+        Backtrack(current + ")", n, open, close + 1, result);
+    }
+
     public IList<string> GenerateParenthesis(int n)
     {
-      return null;
+      if (n == 0)
+        return null;
+
+      var result = new HashSet<string>();
+      Backtrack("", n, 0, 0, result);
+      return result.ToList();
     }
   }
 }
