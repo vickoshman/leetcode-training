965db1be7e7725fd49e709757400601ed402b50f
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-06 12:13:08 +0200

    https://leetcode.com/problems/valid-anagram

---
+++ LeetCode/ValidaAnagramProblem.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace LeetCode
+{
+  public class ValidaAnagramProblem
+  {
+    public bool IsAnagram(string s, string t)
+    {
+      if (s == null || t == null)
+        return false;
+
+      if (s.Length != t.Length)
+        return false;
+
+      var list = s.ToCharArray().ToList();
+      foreach (var c in t)
+      {
+        if (!list.Remove(c))
+          return false;
+      }
+
+      return true;
+    }
+  }
+}
