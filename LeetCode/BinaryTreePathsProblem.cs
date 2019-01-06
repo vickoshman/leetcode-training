f7d079d444f0e0715ae316c72f9a6adee26a5c19
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-06 14:45:14 +0200

    https://leetcode.com/problems/binary-tree-paths

---
+++ LeetCode/BinaryTreePathsProblem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using LeetCode.Trees;
+
+namespace LeetCode
+{
+  public class BinaryTreePathsProblem
+  {
+    private void Traverse(TreeNode root, IList<string> result, string current)
+    {
+      current += root.val;
+
+      if (root.left == null && root.right == null)
+      {
+        result.Add(current);
+        return;
+      }
+
+      current += "->";
+
+      if (root.left != null)
+        Traverse(root.left, result, current);
+
+      if (root.right != null)
+        Traverse(root.right, result, current);
+    }
+
+    public IList<string> BinaryTreePaths(TreeNode root)
+    {
+      if (root == null)
+        return new List<string>();
+
+      var res = new List<string>();
+      Traverse(root, res, "");
+      return res;
+    }
+  }
+}
