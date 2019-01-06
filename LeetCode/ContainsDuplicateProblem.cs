51db24e460777c8d8257a2c08fc2345240adf48e
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-06 11:56:05 +0200

    https://leetcode.com/problems/contains-duplicate

---
+++ LeetCode/ContainsDuplicateProblem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+  public class ContainsDuplicateProblem
+  {
+    public bool ContainsDuplicate(int[] nums)
+    {
+      var set = new HashSet<int>();
+      foreach (var num in nums)
+      {
+        if (!set.Add(num))
+          return true;
+      }
+
+      return false;
+    }
+  }
+}
