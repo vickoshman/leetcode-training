762da3f4375dc62405b4103501c6388ee89e67df
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-06 12:04:12 +0200

    https://leetcode.com/problems/power-of-two

---
+++ LeetCode/PowerOfTwoProblem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode
+{
+  public class PowerOfTwoProblem
+  {
+    public bool IsPowerOfTwo(int n)
+    {
+      if (n < 1)
+        return false;
+
+      while (n > 2)
+      {
+        if (n % 2 != 0)
+          return false;
+
+        n /= 2;
+      }
+
+      return true;
+    }
+  }
+}
