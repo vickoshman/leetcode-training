b83efd26eeb50bab0ef14eed9d1304462968c0ad
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-06 11:53:38 +0200

    https://leetcode.com/problems/intersection-of-two-linked-lists version 2

--- LeetCode/IntersectionOfTwoLinkedListsProblem.cs
+++ LeetCode/IntersectionOfTwoLinkedListsProblem.cs
@@ -1,8 +1,32 @@
-﻿namespace LeetCode
+﻿using System.Collections.Generic;
+
+namespace LeetCode
 {
   public class IntersectionOfTwoLinkedListsProblem
   {
     public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+      var set = new HashSet<ListNode>();
+      var cur = headA;
+      while (cur != null)
+      {
+        set.Add(cur);
+        cur = cur.next;
+      }
+
+      cur = headB;
+      while (cur != null)
+      {
+        if (!set.Add(cur))
+          return cur;
+
+        cur = cur.next;
+      }
+
+      return null;
+    }
+
+    public ListNode GetIntersectionNode2(ListNode headA, ListNode headB)
     {
       var a = headA;
       var b = headB;
