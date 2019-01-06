b1d99c6bb9d3f2458ae4787f1e8ada5962308b7b
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-06 12:07:40 +0200

    https://leetcode.com/problems/delete-node-in-a-linked-list

---
+++ LeetCode/DeleteNodeInALinkedListProblem.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode
+{
+  public class DeleteNodeInALinkedListProblem
+  {
+    public void DeleteNode(ListNode node)
+    {
+      if (node == null || node.next == null)
+        return;
+
+      node.val = node.next.val;
+      node.next = node.next.next;
+    }
+  }
+}
