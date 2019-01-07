9c0b6abcbf891671ddbf4a5410f18ca354e09217
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-07 13:07:06 +0200

    https://leetcode.com/problems/letter-combinations-of-a-phone-number

--- LeetCode/LetterCombinationsOfAPhoneNumberProblem.cs
+++ LeetCode/LetterCombinationsOfAPhoneNumberProblem.cs
@@ -1,34 +1,48 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode
 {
   public class LetterCombinationsOfAPhoneNumberProblem
   {
-    /*public IList<string> LetterCombinations(string digits)
+    Dictionary<int, List<char>> dict = new Dictionary<int, List<char>>
     {
-      var dict = new Dictionary<int, List<char>>
-      {
-        { '2', new List<char> { 'a', 'b', 'c' } },
-        { '3', new List<char> { 'd', 'e', 'f' } },
-        { '4', new List<char> { 'g', 'h', 'i' } },
-        { '5', new List<char> { 'j', 'k', 'l' } },
-        { '6', new List<char> { 'm', 'n', 'o' } },
-        { '7', new List<char> { 'p', 'q', 'r', 's' } },
-        { '8', new List<char> { 't', 'u', 'v' } },
-        { '9', new List<char> { 'w', 'x', 'y', 'z' } }
-      };
+      {'2', new List<char> {'a', 'b', 'c'}},
+      {'3', new List<char> {'d', 'e', 'f'}},
+      {'4', new List<char> {'g', 'h', 'i'}},
+      {'5', new List<char> {'j', 'k', 'l'}},
+      {'6', new List<char> {'m', 'n', 'o'}},
+      {'7', new List<char> {'p', 'q', 'r', 's'}},
+      {'8', new List<char> {'t', 'u', 'v'}},
+      {'9', new List<char> {'w', 'x', 'y', 'z'}}
+    };
 
-      var result = new List<string>();
-      for (int i = 0; i < digits.Length; i++)
+    public void Backtrack(string digits, int digitIndex, char[] snippet, List<string> result)
+    {
+      if (digitIndex == digits.Length)
       {
-        string combination = "";
-        for (int j = 0; j < digits.Length; j++)
-        {
-          if (i == j)
-            continue;
-          
-          combination += digits
-        }
-      }*/
+        result.Add(new string(snippet.ToArray()));
+        return;
+      }
+
+      var digit = digits[digitIndex];
+      var letters = dict[digit];
+      for (int i = 0; i < letters.Count; i++)
+      {
+        snippet[digitIndex] = letters[i];
+        Backtrack(digits, digitIndex + 1, snippet, result);
+      }
     }
+
+    public IList<string> LetterCombinations(string digits)
+    {
+      var result = new List<string>();
+
+      if (digits == null || digits.Length == 0)
+        return result;
+      
+      Backtrack(digits, 0, new char[digits.Length], result);
+      return result;
+    }
+  }
 }
---
+++ LeetCode/SubsetsIIProblem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+  public class SubsetsIIProblem
+  {
+    private static void ExploreSubsets(int[] nums, int offset, List<int> snippet, List<IList<int>> result)
+    {
+      if (offset == nums.Length)
+      {
+        result.Add(new List<int>(snippet));
+        return;
+      }
+
+      snippet.Add(nums[offset]);
+      ExploreSubsets(nums, offset + 1, snippet, result);
+      snippet.RemoveAt(snippet.Count - 1);
+      ExploreSubsets(nums, offset + 1, snippet, result);
+    }
+
+    public IList<IList<int>> SubsetsWithDup(int[] nums)
+    {
+      var result = new List<IList<int>>();
+      ExploreSubsets(nums, 0, new List<int>(), result);
+      return result;
+    }
+  }
+}
