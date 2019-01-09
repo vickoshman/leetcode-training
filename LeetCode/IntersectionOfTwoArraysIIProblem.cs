57cc8f82fab662eeb3e485194bbb92b2e1d1c5f2
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-09 21:35:55 +0200

    https://leetcode.com/problems/intersection-of-two-arrays-ii

---
+++ LeetCode/IntersectionOfTwoArraysIIProblem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+  public class IntersectionOfTwoArraysIIProblem
+  {
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+      Array.Sort(nums1);
+      Array.Sort(nums2);
+
+      var result = new List<int>();
+      int i = 0;
+      int j = 0;
+
+      while (i < nums1.Length && j < nums2.Length)
+      {
+        if (nums1[i] == nums2[j])
+        {
+          result.Add(nums1[i]);
+          i++;
+          j++;
+          continue;
+        }
+
+        if (nums1[i] > nums2[j])
+          j++;
+        else
+          i++;
+      }
+
+      return result.ToArray();
+    }
+  }
+}
