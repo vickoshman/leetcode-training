31d885a15fef15e97b8d95eede4c32f7e033296f
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-09 23:04:38 +0200

    https://leetcode.com/problems/pascals-triangle-ii

---
+++ LeetCode/PascalsTriangleIIProblem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace LeetCode
+{
+  public class PascalsTriangleIIProblem
+  {
+    public IList<int> GetRow(int rowIndex)
+    {
+      if (rowIndex < 0)
+        return null;
+
+      if (rowIndex == 0)
+        return new List<int>() { 1 };
+
+      List<int> prev = null;
+      var current = new List<int>() { 1, 1 };
+
+      for (int i = 2; i < rowIndex + 1; i++)
+      {
+        prev = current;
+        current = new List<int>();
+
+        for (int j = 0; j <= i; j++)
+        {
+          if (j == 0 || j == i)
+          {
+            current.Add(1);
+            continue;
+          }
+
+          current.Add(prev[j - 1] + prev[j]);
+        }
+      }
+
+      return current;
+    }
+  }
+}
