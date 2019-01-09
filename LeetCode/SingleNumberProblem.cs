3ad416cc7186abb7a19fec1f0938e8f6151d18db
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-09 21:48:20 +0200

    https://leetcode.com/problems/single-number/ version 2

--- LeetCode/SingleNumberProblem.cs
+++ LeetCode/SingleNumberProblem.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 
 namespace LeetCode
 {
   public class SingleNumberProblem
   {
-    public int SingleNumber(int[] nums)
+    public int SingleNumber2(int[] nums)
     {
       var set = new HashSet<int>();
       for (int i = 0; i < nums.Length; i++)
@@ -15,6 +16,17 @@ namespace LeetCode
       }
 
       return set.ToArray()[0];
+    }
+
+    public int SingleNumber(int[] nums)
+    {
+      int res = 0;
+      for (int i = 0; i < nums.Length; i++)
+      {
+        res ^= nums[i];
+      }
+
+      return res;
     }
   }
 }
