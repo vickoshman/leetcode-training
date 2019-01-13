f7d33a739c6c1fdbddc4eecc82da608f2655b24c
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-13 15:35:16 +0200

    https://leetcode.com/problems/first-bad-version

---
+++ LeetCode/FirstBadVersionProblem.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode
+{
+  public class FirstBadVersionProblem
+  {
+    private bool IsBadVersion(int num)
+    {
+      return num >= 4;
+    }
+
+    public int FirstBadVersion(int n)
+    {
+      int start = 1;
+      int end = n;
+
+      if (start == end)
+        return start;
+
+      while (true)
+      {
+        if (start > end)
+          return start;
+
+        int middle = start + (end - start) / 2;
+        if (IsBadVersion(middle))
+        {
+          end = middle - 1;
+          continue;
+        }
+
+        start = middle + 1;
+      }
+    }
+  }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,12 +8,7 @@ namespace LeetCode
   {
     static void Main(string[] args)
     {
-      var head = new ListNode(1);
-      head.next = new ListNode(2);
-      head.next.next = new ListNode(3);
-      head.next.next.next = new ListNode(4);
-      head.next.next.next.next = new ListNode(5);
-      var result = new RotateListProblem().RotateRight(head, 5);
+      var result = new FirstBadVersionProblem().FirstBadVersion(5);
 
       Console.WriteLine();
       Console.ReadKey();
