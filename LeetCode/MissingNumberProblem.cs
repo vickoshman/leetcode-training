62ffea57b85071c7b37feeb698c1f3faf15c7700
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-14 22:42:06 +0200

    https://leetcode.com/problems/missing-number

---
+++ LeetCode/MissingNumberProblem.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode
+{
+  public class MissingNumberProblem
+  {
+    public int MissingNumber(int[] nums)
+    {
+      int s = 0;
+      int sum = 0;
+
+      for (int i = 0; i < nums.Length; i++)
+      {
+        sum += nums[i];
+        s += i;
+      }
+
+      return s+nums.Length - sum;
+    }
+  }
+}
