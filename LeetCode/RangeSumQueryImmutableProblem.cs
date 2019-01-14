9d60f68efe45f594c30090327dc692cb63cb1870
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-14 22:12:31 +0200

    https://leetcode.com/problems/range-sum-query-immutable

---
+++ LeetCode/RangeSumQueryImmutableProblem.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode
+{
+  public class RangeSumQueryImmutableProblem
+  {
+    private readonly int[] _nums;
+    public NumArray(int[] nums)
+    {
+      _nums = nums;
+    }
+
+    public int SumRange(int i, int j)
+    {
+      if (_nums == null)
+        return 0;
+
+      if (i < 0 || j >= _nums.Length || i > j)
+        return 0;
+
+      int sum = 0;
+      for (int index = i; index <= j; index++)
+        sum += _nums[index];
+
+      return sum;
+    }
+  }
+}
