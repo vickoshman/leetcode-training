060342e85a1ef1b256d24a57a9ebd75449928712
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-14 22:29:19 +0200

    https://leetcode.com/problems/find-the-difference

---
+++ LeetCode/FindTheDifferencePRoblem.cs
@@ -0,0 +1,32 @@
+﻿using System.CodeDom;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+  public class FindTheDifferenceProblem
+  {
+    public char FindTheDifference(string s, string t)
+    {
+      var dic = new Dictionary<char, int>();
+      foreach (var c in s)
+      {
+        if (dic.ContainsKey(c))
+          dic[c]++;
+        else
+          dic.Add(c, 1);
+      }
+
+      foreach (var c in t)
+      {
+        if (!dic.ContainsKey(c))
+          return c;
+
+        dic[c]--;
+        if (dic[c] < 0)
+          return c;
+      }
+
+      return ' ';
+    }
+  }
+}
