b7287fbe18aa896df6a9b405c664169ac9fca9fb
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-16 20:40:37 +0200

    https://leetcode.com/problems/symmetric-tree

---
+++ LeetCode/SymmetricTreeProblem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Runtime.Serialization.Formatters;
+using LeetCode.Trees;
+
+namespace LeetCode
+{
+  public class SymmetricTreeProblem
+  {
+    public bool IsSymmetric(TreeNode root)
+    {
+      if (root == null)
+        return true;
+
+      return Is(root.left, root.right);
+    }
+
+    private bool Is(TreeNode left, TreeNode right)
+    {
+      if (left == null || right == null)
+        return left == right;
+
+      if (left.val != right.val)
+        return false;
+
+      return Is(left.left, right.right) && Is(left.right, right.left);
+    }
+  }
+}
