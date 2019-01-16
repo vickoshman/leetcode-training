a5ec3affabe4ff4b41829716fe64553f30c34c82
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-16 21:02:19 +0200

    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted

---
+++ LeetCode/TwoSumInputArrayIsSortedPRoblem.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace LeetCode
+{
+  public class TwoSumInputArrayIsSortedProblem
+  {
+    public int[] TwoSum(int[] numbers, int target)
+    {
+      for (int i = 0; i < numbers.Length - 1; i++)
+      {
+        for (int j = numbers.Length - 1; j > i; j--)
+        {
+          if (numbers[i] + numbers[j] == target)
+            return new int[] {i + 1, j + 1};
+        }
+      }
+
+      return null;
+    }
+  }
+}
