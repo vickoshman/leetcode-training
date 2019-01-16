28c7501a1ec42a6e7bac632271ba957f47bdd53a
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-16 20:49:03 +0200

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock

---
+++ LeetCode/BestTimeToBuyAndSellStockProblem.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace LeetCode
+{
+  public class BestTimeToBuyAndSellStockProblem
+  {
+    public int MaxProfit(int[] prices)
+    {
+      int maxProfit = 0;
+      
+      for (int i = 0; i < prices.Length - 1; i++)
+      {
+        for (int j = i + 1; j < prices.Length; j++)
+        {
+          var profit = prices[j] - prices[i];
+          if (profit > maxProfit)
+            maxProfit = profit;
+        }
+      }
+
+      return maxProfit;
+    }
+  }
+}
