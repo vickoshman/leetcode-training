f79782bc36cc26c798d01d700938b2dd0652b9ab
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-19 10:37:46 +0200

    https://leetcode.com/problems/contains-duplicate-ii

---
+++ LeetCode/ContainsDuplicateIIProblem.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode
+{
+  public class ContainsDuplicateIIProblem
+  {
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+      for (int i = 0; i < nums.Length - 1; i++)
+      {
+        for (int j = i + 1; j < i + k + 1 && j < nums.Length; j++)
+        {
+          if (nums[i] == nums[j])
+            return true;
+        }
+      }
+
+      return false;
+    }
+  }
+}
