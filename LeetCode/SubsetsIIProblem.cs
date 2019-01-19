d0fcb3fdd4fd5669639fce2879e99057281f246c
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-19 13:08:28 +0200

    https://leetcode.com/problems/subsets-ii

--- LeetCode/SubsetsIIProblem.cs
+++ LeetCode/SubsetsIIProblem.cs
@@ -1,28 +1,37 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode
 {
   public class SubsetsIIProblem
   {
-    private static void ExploreSubsets(int[] nums, int offset, List<int> snippet, List<IList<int>> result)
+    private void Explore(int[] nums, int from, List<int> soFar, Dictionary<string, IList<int>> result)
     {
-      if (offset == nums.Length)
+      if (from >= nums.Length)
       {
-        result.Add(new List<int>(snippet));
+        var copy = soFar.ToList();
+        copy.Sort();
+        var key = string.Join(":", copy);
+        if (!result.ContainsKey(key))
+          result.Add(key, soFar.ToArray());
+
         return;
       }
 
-      snippet.Add(nums[offset]);
-      ExploreSubsets(nums, offset + 1, snippet, result);
-      snippet.RemoveAt(snippet.Count - 1);
-      ExploreSubsets(nums, offset + 1, snippet, result);
+      for (int i = from; i < nums.Length; i++)
+      {
+        soFar.Add(nums[i]);
+        Explore(nums, i + 1, soFar, result);
+        soFar.RemoveAt(soFar.Count - 1);
+        Explore(nums, i + 1, soFar, result);
+      }
     }
 
     public IList<IList<int>> SubsetsWithDup(int[] nums)
     {
-      var result = new List<IList<int>>();
-      ExploreSubsets(nums, 0, new List<int>(), result);
-      return result;
+      var result = new Dictionary<string, IList<int>>();
+      Explore(nums, 0, new List<int>(), result);
+      return result.Values.ToList();
     }
   }
 }
