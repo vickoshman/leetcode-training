fd893ce06d238a4628cb1833b425a624421cd3c2
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-19 12:24:25 +0200

    https://leetcode.com/problems/binary-tree-postorder-traversal

---
+++ LeetCode/BinaryTreePostorderTraversalProblem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using LeetCode.Trees;
+
+namespace LeetCode
+{
+  public class BinaryTreePostorderTraversalProblem
+  {
+    private void Traverse(TreeNode root, List<int> result)
+    {
+      if (root.left != null)
+        Traverse(root.left, result);
+
+      if (root.right != null)
+        Traverse(root.right, result);
+
+      result.Add(root.val);
+    }
+
+    public IList<int> PostorderTraversal(TreeNode root)
+    {
+      var result = new List<int>();
+      if (root == null)
+        return result;
+
+      Traverse(root, result);
+      return result;
+    }
+  }
+}
