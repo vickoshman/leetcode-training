14d5fb298abef084921d4abc3c54219d38639c0b
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-19 13:18:05 +0200

    https://leetcode.com/problems/subsets-ii version 2

--- LeetCode/SubsetsIIProblem.cs
+++ LeetCode/SubsetsIIProblem.cs
@@ -1,37 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LeetCode
 {
   public class SubsetsIIProblem
   {
-    private void Explore(int[] nums, int from, List<int> soFar, Dictionary<string, IList<int>> result)
+    private void Explore(int[] nums, int from, List<int> soFar, List<IList<int>> result)
     {
+      result.Add(soFar.ToList());
       if (from >= nums.Length)
       {
-        var copy = soFar.ToList();
-        copy.Sort();
-        var key = string.Join(":", copy);
-        if (!result.ContainsKey(key))
-          result.Add(key, soFar.ToArray());
-
         return;
       }
 
+      var used = new HashSet<int>();
       for (int i = from; i < nums.Length; i++)
       {
+        if (used.Contains(nums[i]))
+          continue;
+
         soFar.Add(nums[i]);
         Explore(nums, i + 1, soFar, result);
         soFar.RemoveAt(soFar.Count - 1);
-        Explore(nums, i + 1, soFar, result);
+        used.Add(nums[i]);
       }
     }
 
     public IList<IList<int>> SubsetsWithDup(int[] nums)
     {
-      var result = new Dictionary<string, IList<int>>();
+      Array.Sort(nums);
+      var result = new List<IList<int>>();
       Explore(nums, 0, new List<int>(), result);
-      return result.Values.ToList();
+      return result;
     }
   }
 }
