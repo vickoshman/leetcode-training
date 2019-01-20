b98c62936ff1d8496cd269024d8bdad714e13ac5
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-20 17:36:27 +0200

    https://leetcode.com/problems/isomorphic-strings

---
+++ LeetCode/IsIsomorphicStringsProblem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+  public class IsIsomorphicStringsProblem
+  {
+    public bool IsIsomorphic(string s, string t)
+    {
+      var map = new Dictionary<char, char>();
+      var set = new HashSet<char>();
+
+      for (int i = 0; i < s.Length; i++)
+      {
+        if (map.ContainsKey(s[i]))
+        {
+          if (t[i] != map[s[i]])
+            return false;
+        }
+        else
+        {
+          if (set.Contains(t[i]))
+            return false;
+
+          map.Add(s[i], t[i]);
+          set.Add(t[i]);
+        }
+      }
+
+      return true;
+    }
+  }
+}
