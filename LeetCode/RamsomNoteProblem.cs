0cd0f77589089354839f44afbc46dae28b2ef15c
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-20 12:33:04 +0200

    https://leetcode.com/problems/ransom-note

---
+++ LeetCode/RamsomNoteProblem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+  public class RamsomNoteProblem
+  {
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+      if (ransomNote.Length > magazine.Length)
+        return false;
+
+      var list = magazine.ToCharArray().ToList();
+      for (int i = 0; i < ransomNote.Length; i++)
+      {
+        if (!list.Remove(ransomNote[i]))
+          return false;
+      }
+
+      return true;
+    }
+  }
+}
