15d33b6283a6498f950c25965a40314e61959459
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-20 12:26:49 +0200

    https://leetcode.com/problems/reverse-words-in-a-string

---
+++ LeetCode/ReverseWordsInAStringProblem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+  public class ReverseWordsInAStringProblem
+  {
+    public string ReverseWords(string s)
+    {
+      var stack = new Stack<string>();
+      string word = "";
+      for (int i = 0; i < s.Length; i++)
+      {
+        if (s[i] == ' ')
+        {
+          if (string.IsNullOrEmpty(word))
+            continue;
+
+          stack.Push(word);
+          word = "";
+          continue;
+        }
+
+        word += s[i];
+      }
+
+      if (!string.IsNullOrEmpty(word))
+        stack.Push(word);
+
+      var result = new StringBuilder();
+
+      while (stack.Count > 0)
+      {
+        result.Append(stack.Pop());
+        if (stack.Count == 0)
+          return result.ToString();
+
+        result.Append(" ");
+      }
+
+      return "";
+    }
+  }
+}
