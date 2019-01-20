d4b64d25145feb18cf67b6575fe42710b4828386
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-20 13:42:57 +0200

    https://leetcode.com/problems/3sum-closest

---
+++ LeetCode/ThreeSumClosestProblem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode
+{
+  public class ThreeSumClosestProblem
+  {
+    public int ThreeSumClosest(int[] nums, int target)
+    {
+      int closest = nums[0] + nums[1] + nums[2];
+
+      for (int i = 0; i < nums.Length; i++)
+      {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+          for (int k = j + 1; k < nums.Length; k++)
+          {
+            int sum = nums[i] + nums[j] + nums[k];
+            int prevDif = Math.Abs(closest - target);
+            int currentDif = Math.Abs(sum - target);
+
+            if (currentDif == 0)
+              return sum;
+
+            if (currentDif < prevDif)
+              closest = sum;
+          }
+        }
+      }
+
+      return closest;
+    }
+  }
+}
