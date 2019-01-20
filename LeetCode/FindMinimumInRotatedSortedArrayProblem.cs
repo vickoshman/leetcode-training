a165f49fee52b6d6ff086683e379566c051e19d3
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-20 14:12:32 +0200

    https://leetcode.com/problems/find-minimum-in-rotated-sorted-array

---
+++ LeetCode/FindMinimumInRotatedSortedArrayProblem.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode
+{
+  public class FindMinimumInRotatedSortedArrayProblem
+  {
+    public int FindMin(int[] nums)
+    {
+      int min = nums[0];
+      for (int i = 1; i < nums.Length; i++)
+      {
+        if (nums[i] < min)
+          min = nums[i];
+      }
+
+      return min;
+    }
+  }
+}
