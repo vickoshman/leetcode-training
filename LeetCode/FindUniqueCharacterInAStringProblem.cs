1346b71e0cfaf392f2ee0efe7be2b187176f4706
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-20 16:39:38 +0200

    https://leetcode.com/problems/first-unique-character-in-a-string

---
+++ LeetCode/FindUniqueCharacterInAStringProblem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+  public class FindUniqueCharacterInAStringProblem
+  {
+    public int FirstUniqChar(string s)
+    {
+      if (s.Length == 1)
+        return 0;
+
+      var d = new Dictionary<char, int>();
+      for (int i = 0; i < s.Length; i++)
+      {
+        if (d.ContainsKey(s[i]))
+          d[s[i]]++;
+        else
+          d.Add(s[i], 1);
+      }
+
+      char c = ' ';
+      foreach (var kvp in d)
+      {
+        if (kvp.Value == 1)
+        {
+          c = kvp.Key;
+          break;
+        }
+      }
+
+      return s.IndexOf(c);
+    }
+  }
+}
