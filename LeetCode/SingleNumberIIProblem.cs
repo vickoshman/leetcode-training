fdc11205843bfb578cbc698a1466ce35ffac1efa
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-20 11:42:24 +0200

    https://leetcode.com/problems/single-number-ii

---
+++ LeetCode/SingleNumberIIProblem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+  public class SingleNumberIIProblem
+  {
+    public int SingleNumber(int[] nums)
+    {
+      var dic = new Dictionary<int, int>();
+      for (int i = 0; i < nums.Length; i++)
+      {
+        if (dic.ContainsKey(nums[i]))
+          dic[nums[i]]++;
+        else
+          dic.Add(nums[i], 1);
+      }
+
+      foreach (var kvp in dic)
+      {
+        if (kvp.Value == 1)
+          return kvp.Key;
+      }
+
+      return -1;
+    }
+  }
+}
