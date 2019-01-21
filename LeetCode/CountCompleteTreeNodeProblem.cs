03387b5d6765f981b59adff2e4de50fdfd2f87d9
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-21 08:32:35 +0200

    https://leetcode.com/problems/count-complete-tree-nodes

---
+++ LeetCode/CountCompleteTreeNodeProblem.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.AccessControl;
+using LeetCode.Trees;
+
+namespace LeetCode
+{
+  public class CountCompleteTreeNodeProblem
+  {
+    private int Count(TreeNode node)
+    {
+      int sum = 0;
+
+      if (node != null)
+        sum++;
+      else
+        return sum;
+
+      if (node.left != null)
+      {
+        sum += Count(node.left);
+      }
+
+      if (node.right != null)
+      {
+        sum += Count(node.right);
+      }
+
+      return sum;
+    }
+
+    public int CountNodes(TreeNode root)
+    {
+      return Count(root);
+    }
+  }
+}
---
+++ LeetCode/ReorderListProblem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+  public class ReorderListProblem
+  {
+    public void ReorderList(ListNode head)
+    {
+      if (head == null)
+        return;
+
+      ListNode mid;
+      var fast = head;
+      var slow = head;
+
+      while (fast != null && fast.next != null)
+      {
+        fast = fast.next.next;
+        slow = slow.next;
+      }
+
+      mid = slow.next;
+      slow.next = null;
+
+      var l1 = head;
+
+      var current = head;
+      ListNode prev = null;
+      while (current != null)
+      {
+        var nextNode = current.next;
+        current.next = prev;
+        prev = current;
+        current = nextNode;
+      }
+
+      var l2 = prev;
+
+      while (l2 != null)
+      {
+        var n1 = l1.next;
+        var n2 = l2.next;
+
+        l1.next = l2;
+        l2.next = n1;
+
+        l1 = n1;
+        l2 = n2;
+      }
+    }
+  }
+}
