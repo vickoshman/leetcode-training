9b77cde2fbcf1967cc439b827e9ce1fdf89aeb6f
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-22 10:03:13 +0200

    https://leetcode.com/problems/search-in-rotated-sorted-array

--- LeetCode/ExcelSheetColumnTitleProblem.cs
+++ LeetCode/ExcelSheetColumnTitleProblem.cs
@@ -7,12 +7,6 @@ namespace LeetCode
     public string ConvertToTitle(int n)
     {
       int baseCode = 65;
-      int sum = 0;
-
-      while (n > 0)
-      {
-        int current = n % 26;
-      }
 
       return "";
     }
--- LeetCode/SearchInRotatedSortedArrayProblem.cs
+++ LeetCode/SearchInRotatedSortedArrayProblem.cs
@@ -2,21 +2,82 @@
 {
   public class SearchInRotatedSortedArrayProblem
   {
-    private int FindPivotIndex(int[] nums, int from, int to)
-    {
-      if (from != 0 && nums[from] < nums[from - 1])
-        return from;
-
-      if (nums[from] > nums[0])
-        return FindPivotIndex(nums, (from + to) / 2, to);
-      else
-        return FindPivotIndex(nums, (from + to) / 2, to);
-    }
-
     public int Search(int[] nums, int target)
     {
-      int pivot = FindPivotIndex(nums, 0, nums.Length - 1);
-      return pivot;
+      if (nums.Length == 0)
+        return -1;
+
+      if (nums.Length == 1)
+        return nums[0] == target ? 0 : -1;
+
+      if (nums.Length == 2)
+        return nums[0] == target ? 0 : nums[1] == target ? 1 : -1;
+
+      int arrayStartIndex = -1;
+      if (nums[0] < nums[nums.Length - 1])
+        arrayStartIndex = 0;
+      else
+      {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+          int mid = left + (right - left) / 2;
+          if (nums[mid] > nums[mid + 1])
+          {
+            arrayStartIndex = mid + 1;
+            break;
+          }
+
+          if (nums[mid - 1] > nums[mid])
+          {
+            arrayStartIndex = mid;
+            break;
+          }
+
+          if (nums[0] < nums[mid])
+            left = mid + 1;
+          else
+            right = mid - 1;
+        }
+      }
+
+      if (nums[arrayStartIndex] == target)
+        return arrayStartIndex;
+
+      int l = 0;
+      int r = 0;
+
+      if (target > nums[nums.Length - 1])
+      {
+        l = 0;
+        r = arrayStartIndex - 1;
+      }
+      else
+      {
+        l = arrayStartIndex;
+        r = nums.Length;
+      }
+
+      while (l <= r)
+      {
+        int mid = l + (r - l) / 2;
+
+        if (nums[mid] == target)
+          return mid;
+
+        if (nums[mid] > target)
+        {
+          r = mid - 1;
+        }
+        else
+        {
+          l = mid + 1;
+        }
+      }
+
+      return -1;
     }
   }
 }
