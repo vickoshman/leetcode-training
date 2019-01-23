4ff6b6f6ccf07dfedbdf2abaa6ba6c72227873fe
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-23 07:55:24 +0200

    https://leetcode.com/problems/binary-tree-preorder-traversal

---
+++ LeetCode/BinaryTreeProorderTraversalProblem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Security.AccessControl;
+using LeetCode.Trees;
+
+namespace LeetCode
+{
+  public class BinaryTreeProorderTraversalProblem
+  {
+    private void Traverse(TreeNode node, List<int> result)
+    {
+      if (node == null)
+        return;
+
+      result.Add(node.val);
+      if (node.left != null)
+        Traverse(node.left, result);
+
+      if (node.right != null)
+        Traverse(node.right , result);
+    }
+
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+      var res = new List<int>();
+      //Traverse(root, res);
+
+      if (root == null)
+        return res;
+
+      var stack = new Stack<TreeNode>();
+      stack.Push(root);
+
+      while (stack.Count > 0)
+      {
+        var node = stack.Pop();
+        res.Add(node.val);
+
+        if (node.right != null)
+          stack.Push(node.right);
+
+        if (node.left != null)
+          stack.Push(node.left);
+      }
+
+      return res;
+    }
+  }
+}
