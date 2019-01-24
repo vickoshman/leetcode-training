a7bd7877152417bf0cc7d0be658e4ca3c5f94334
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-24 07:14:58 +0200

    https://leetcode.com/problems/container-with-most-water

---
+++ LeetCode/ContainerWithMostWaterProblem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace LeetCode
+{
+  public class ContainerWithMostWaterProblem
+  {
+    public int MaxArea(int[] height)
+    {
+      int max = 0;
+
+      for (int i = 0; i < height.Length - 1; i++)
+      {
+        for (int j = i + 1; j < height.Length; j++)
+        {
+          int h = Math.Min(height[i], height[j]);
+          int l = j - i;
+
+          var area = h * l;
+          if (area > max)
+            max = area;
+        }
+      }
+
+      return max;
+    }
+  }
+}
