c2a0b69620c1ab2e71eb44230e690242423a5453
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-26 14:05:58 +0200

    https://leetcode.com/problems/single-number-iii

---
+++ LeetCode/SingleNumberIIIProblem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace LeetCode
+{
+  public class SingleNumberIIIProblem
+  {
+    public int[] SingleNumber(int[] nums)
+    {
+      var set = new HashSet<int>();
+
+      for (int i = 0; i < nums.Length; i++)
+      {
+        if (!set.Add(nums[i]))
+          set.Remove(nums[i]);
+      }
+
+      return set.ToArray();
+    }
+  }
+}
