4bef693285ad461f48a6401e5dc95704a8643048
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-26 11:06:06 +0200

    https://leetcode.com/problems/maximum-product-subarray

---
+++ LeetCode/MaximumProductSubarrayProblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode
+{
+  public class MaximumProductSubarrayProblem
+  {
+    public int MaxProduct(int[] nums)
+    {
+      int max = int.MinValue;
+      int current = 1;
+
+      for (int i = 0; i < nums.Length; i++)
+      {
+        for (int j = i; j < nums.Length; j++)
+        {
+          current *= nums[j];
+          if (current > max)
+            max = current;
+        }
+
+        current = 1;
+      }
+
+      return max;
+    }
+  }
+}
