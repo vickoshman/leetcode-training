bb7c525f87806b9b468a8ddca5dd8be36768665b
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-26 10:48:38 +0200

    https://leetcode.com/problems/number-of-islands

--- LeetCode/FindAllNumberDisappearedInAnArrayProblem.cs
+++ LeetCode/FindAllNumberDisappearedInAnArrayProblem.cs
@@ -14,9 +14,14 @@ namespace LeetCode
         return result;
 
       Array.Sort(nums);
-      
+
+      int searched = -1;
       for (int i = 1; i <= nums.Length; i++)
       {
+        if (searched != -1 && i == searched)
+          continue;
+
+        searched = i;
         int pos = Array.BinarySearch(nums, i);
         if (pos < 0)
           result.Add(i);
---
+++ LeetCode/NumIslandsProblem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace LeetCode
+{
+  public class NumIslandsProblem
+  {
+    public int NumIslands(char[,] grid)
+    {
+      var visited = new HashSet<string>();
+      int islands = 0;
+
+      for (int i = 0; i < grid.GetLength(0); i++)
+        for (int j = 0; j < grid.GetLength(1); j++)
+        {
+          int before = visited.Count;
+          Traverse(grid, i, j, visited);
+          if (before != visited.Count)
+            islands++;
+        }
+
+      return islands;
+    }
+
+    private void Traverse(char[,] grid, int i, int j, HashSet<string> visited)
+    {
+      if (i < 0 || i >= grid.GetLength(0))
+        return;
+
+      if (j < 0 || j >= grid.GetLength(1))
+        return;
+
+      if (grid[i, j] == '0')
+        return;
+
+      if (!visited.Add(i + ":" + j))
+        return;
+
+      Traverse(grid, i - 1, j, visited);
+      Traverse(grid, i + 1, j, visited);
+
+      Traverse(grid, i, j - 1, visited);
+      Traverse(grid, i, j + 1, visited);
+    }
+  }
+}
