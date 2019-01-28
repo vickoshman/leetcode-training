6f8ea6983fbf36a9f7a1050785e8b3ff0ca7c6b5
Victor Koshman <victor.a.koshman@gmail.com>
2019-01-28 08:17:22 +0200

    https://www.hackerrank.com/challenges/migratory-birds

---
+++ HackerRank/MigratoryBirdsProblem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank
+{
+  public class MigratoryBirdsProblem
+  {
+    static int migratoryBirds(List<int> arr)
+    {
+      var dic = new Dictionary<int, int>();
+      foreach (var element in arr)
+      {
+        if (dic.ContainsKey(element))
+          dic[element]++;
+        else
+          dic.Add(element, 1);
+      }
+
+      int maxKey = 0;
+      int maxValue = 0;
+      foreach (var kvp in dic)
+      {
+        if (kvp.Value > maxValue)
+        {
+          maxKey = kvp.Key;
+          maxValue = kvp.Value;
+          continue;
+        }
+
+        if (kvp.Value == maxValue && kvp.Key < maxKey)
+        {
+          maxKey = kvp.Key;
+          maxValue = kvp.Value;
+          continue;
+        }
+      }
+
+      return maxKey;
+    }
+  }
+}
---
+++ HackerRank/RepeatedStringProblem.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace HackerRank
+{
+  public class RepeatedStringProblem
+  {
+    static long repeatedString(string s, long n)
+    {
+      int num = 0;
+      for (int i = 0; i < s.Length; i++)
+        if (s[i] == 'a')
+          num++;
+
+      return num * n - (n-s.Length);
+    }
+  }
+}
