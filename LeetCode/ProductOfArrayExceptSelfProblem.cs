dfcde2eb963b2a8cec390d0b2a2499d040587f6f
Victor Koshman <victor.a.koshman@gmail.com>
2019-02-05 08:10:31 +0200

    https://leetcode.com/problems/product-of-array-except-self

---
+++ LeetCode/ProductOfArrayExceptSelfProblem.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+  public class ProductOfArrayExceptSelfProblem
+  {
+    public int[] ProductExceptSelf(int[] nums)
+    {
+      var res = new int[nums.Length];
+
+      res[0] = 1;
+      for (int i = 1; i < nums.Length; i++)
+      {
+        res[i] = res[i - 1] * nums[i - 1];
+      }
+
+      int right = 1;
+      for (int i = nums.Length - 1; i >= 0; i--)
+      {
+        res[i] *= right;
+        right *= nums[i];
+      }
+
+      return res;
+    }
+  }
+}
--- LeetCode/SummaryRangesProblem.cs
+++ LeetCode/SummaryRangesProblem.cs
@@ -10,37 +10,30 @@ namespace LeetCode
     {
       var res = new List<string>();
 
-      if (nums.Length == 0)
-        return res;
+      int prev = nums[0];
+      var sb = new StringBuilder(prev);
+      int numbers = 0;
 
-      if (nums.Length == 1)
-        return new List<string>() { nums[0].ToString()};
-
-      var prev = nums[0];
-      var rangeStart = prev;
-
-      for (int i = 1; i < nums.Length; i++)
+      for (int i = 0; i < nums.Length; i++)
       {
-        var current = nums[i];
-
-        if (current == prev + 1)
+        if (nums[i] == prev + 1)
         {
-          prev = current;
+          prev = nums[i];
           continue;
         }
 
-        if (rangeStart == prev)
-        {
-          res.Add(rangeStart.ToString());
-          i--;
-        }
-        else
-        {
-          res.Add(rangeStart + "->" + prev);
-        }
+        if (sb.Length > 0)
+          sb.Append("->");
 
-        rangeStart = current;
-        prev = current;
+        sb.Append(prev);
+        numbers++;
+
+        if (numbers == 2)
+        {
+          res.Add(sb.ToString());
+          sb.Clear();
+          numbers = 0;
+        }
       }
 
       return res;
