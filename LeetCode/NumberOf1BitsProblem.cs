70834e778f8341ed6860f8230c14f5887c990453
Victor Koshman <victor.a.koshman@gmail.com>
2019-02-05 07:34:28 +0200

    https://leetcode.com/problems/number-of-1-bits

---
+++ LeetCode/NumberOf1BitsProblem.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+  public class NumberOf1BitsProblem
+  {
+    public int HammingWeight(uint n)
+    {
+      int mask = 1;
+      int count = 0;
+
+      for (int i = 0; i < 32; i++)
+      {
+        if ((n & mask) != 0)
+          count++;
+
+        mask <<= 1;
+      }
+
+      return count;
+    }
+  }
+}
